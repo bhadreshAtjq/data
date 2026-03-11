22a5d3a1e3fdb7182942726d55ad2c9dc41a29ea
bhadreshAtjq <bhadresh@jadequest.com>
2026-03-11 15:37:35 +0530

    Add introductory guide to IPFS concepts
    Added an introductory guide to IPFS, explaining its concepts and distinguishing it from storage and cloud service providers.

---
+++ dataset.tsx
@@ -0,0 +1,20 @@
+What is IPFS
+IPFS is a set of building blocks for a better web. Open protocols to make your data smarter: content-addressed, verifiable, and unstoppable.
+
+On a more technical level, IPFS is a set of open protocols for addressing, routing, and transferring data on the web, built on the ideas of content addressing and peer-to-peer networking.
+
+This guide is part 1 of a 3-part introduction to the basic concepts of IPFS. The second part, IPFS and the problems it solves, covers the problems with the internet and current protocols like HTTP that IPFS solves.
+
+In this conceptual guide, you'll learn what IPFS is and isn't.
+
+Defining IPFS
+The term IPFS can refer to multiple concepts:
+
+An implementation of IPFS protocol specifications (opens new window), such as Kubo. Learn more about the principles that define an IPFS implementation.
+A decentralized network composed of IPFS nodes that is open and participatory.
+A modular suite of protocols and standards for organizing and transferring content-addressed data.
+What IPFS isn't
+While IPFS shares similarities with, and is often used in architectures with the systems described below, IPFS is not:
+
+A storage provider: While there are storage providers built with IPFS support (typically known as pinning services), IPFS itself is a protocol, not a provider.
+A cloud service provider: IPFS can be deployed on and complement cloud infrastructure, but it in of itself is not a cloud service provider.
